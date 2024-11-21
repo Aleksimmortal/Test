88fc28f53f727ee25561864d0a5cdb9ce8d315a0
Антон <aleksimmortal@mail.ru>
2024-11-21 13:13:20 +0300

    Added triangle

--- Program.cs
+++ Program.cs
@@ -19,4 +19,17 @@ for (int i = 0;i < mas.Length;i++)
 {
     mas[i] = i;
     Console.WriteLine(i);
-}   
+}
+
+Console.WriteLine("Введите высоту треугольника:  ");
+int height = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = height; j > i; j--)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+Console.ReadLine();
